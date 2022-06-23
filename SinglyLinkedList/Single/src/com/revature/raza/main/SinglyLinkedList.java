efe8199e1f4b14ba0c1e0d8bee8b11f86baa6e49
raza <raza.ghulam2021@gmail.com>
2022-06-23 10:40:08 -0700

    Added a body to add metod of SinglyLinkedList class

--- SinglyLinkedList/Single/src/com/revature/raza/main/SinglyLinkedList.java
+++ SinglyLinkedList/Single/src/com/revature/raza/main/SinglyLinkedList.java
@@ -5,12 +5,32 @@ class SinglyLinkedList implements List {
 	class Node {
 		Object data; 
 		Node nextNode; 
+		
+		Node(Object data) {
+			this.data = data; 
+			nextNode = null; 
+		}
 	}
 
+	Node head; 
+	
 	@Override
+	//Method to add objects 
 	public void add(Object obj) {
 		// TODO Auto-generated method stub
+		Node node = new Node(obj); 
 		
+		if (head == null) {
+			head = node; 
+		} else {
+			
+			Node current = head; 
+			while (current.nextNode != null) {
+				current= current.nextNode; 
+			}
+			current.nextNode = node; 
+			head = current; 	
+		}
 	}
 
 	@Override
